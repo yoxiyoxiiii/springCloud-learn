95ff20afcb5751270d73cd39e35362a97afa1265
蒋焰 <846814595@qq.com>
2019-02-21 18:53:58 +0800

    dash

--- cloud-hystrix-dashboard/src/main/java/com/example/cloudhystrixdashboard/CloudHystrixDashboardApplication.java
+++ cloud-hystrix-dashboard/src/main/java/com/example/cloudhystrixdashboard/CloudHystrixDashboardApplication.java
@@ -1,13 +1,16 @@
 package com.example.cloudhystrixdashboard;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +44,19 @@ public class CloudHystrixDashboardApplication {
 
 	public String hiError(String name) {
 		return "hi,"+name+",sorry,error!";
+	}
+
+	/**
+	 * 解决异常连接
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 }
