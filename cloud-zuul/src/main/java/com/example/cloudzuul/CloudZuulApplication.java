815e8d98afa150afb3ec556990eae55613f7e05f
846814595@qq.com <846814595@qq.com>
2019-02-26 11:08:39 +0800

    dashbord

--- cloud-zuul/src/main/java/com/example/cloudzuul/CloudZuulApplication.java
+++ cloud-zuul/src/main/java/com/example/cloudzuul/CloudZuulApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 /**
  * @EnableZuulProxy 启用 zuul 网关
